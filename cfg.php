7684ac69be153e575771bc52c7e048a8aec53274
eftechanel <46001373+eftechanel@users.noreply.github.com>
2019-02-09 20:36:34 +0700

    Add files via upload

---
+++ cfg.php
@@ -0,0 +1,16 @@
+<?php
+
+/* Seting opsiaonal */
+$delay=30;
+// multiple account update VIP+
+// akun utama nomor satu
+// jika kalian mau menambahkan akun ikuti contoh di bawah
+// Ganti xxx punya ente
+
+$config = [
+//Akun 1
+"ticket=XXXXXXXXX&meid=XXXXXXX",
+
+];
+
+?>
